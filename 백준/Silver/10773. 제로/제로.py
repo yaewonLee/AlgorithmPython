34386a0094d6326f7a38e4602990a45d11ae7566
Yaewon Lee <yw990324@gmail.com>
2025-04-25 22:39:57 +0900

    [Silver IV] Title: 제로, Time: 2464 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,11 @@
+K = int(input())
+stack = []
+
+for _ in range(K):
+    number = int(input())
+    if number == 0:
+        stack.pop()
+    else:
+        stack.append(number)
+
+print(sum(stack))
